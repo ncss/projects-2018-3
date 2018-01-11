18f8a784e6d5bbb15cd07b26648dc79c4513bdfd
iseaceus <iseaceus@gmail.com>
2018-01-11 17:44:06 +1100

    Adds render_file method
    The tests are really bad

--- server.py
+++ server.py
@@ -3,6 +3,21 @@ import tornadotesting
 from template import render_template
 from db import User
 
+def render_file(filename, context):
+    """
+    Reads a template file and pases it through the template engine.
+
+    >>>tornadotesting.run(render_file, 'profile.html', '{"name":"sandy"}')
+    My name is sandy
+
+    """
+    with open(filename) as f:
+
+        template = f.read()
+
+        return render_template(template, context)
+
+
 def view_profile(request, username):
     """
     >>> tornadotesting.run(view_profile, 'alice')
@@ -14,7 +29,7 @@ def view_profile(request, username):
     template = "My username is {{ name }}"
     context = {'name':user.username}
 
-    request.write(render_template(template, context))
+    request.write(render_file('profile.html', context))
 
 
 
