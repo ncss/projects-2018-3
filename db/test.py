e05f095e8baa84e2d4d8144c339f1fc78283dc0b
snudmeh <snudmeh@hotmail.com>
2018-01-11 16:19:48 +1100

    Created User class with method "get_all_users"

---
+++ db/test.py
@@ -0,0 +1,16 @@
+from db import User
+import unittest
+
+class Testing(unittest.TestCase):
+	#def __init__(self):
+	#	super(unittest.TestCase).__init__()
+	#	pass
+		
+	def test_get_all_users(self):
+		user = User()
+		result = user.get_all_users()
+		#user = User()
+		self.assertEqual(result[0].username, user.username)
+		
+if __name__ == '__main__':
+	unittest.main()
