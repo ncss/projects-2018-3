8f0003ed3eee359179e070576f014bbe155c24ba
Angeni Bai <angenibai@gmail.com>
2018-01-11 17:42:22 +1100

    parse text and beginning of parse group
    Not tested

--- template.py
+++ template.py
@@ -1,3 +1,5 @@
+from templatingNodes import *
+
 def render_template(string, context):
     """
     >>> render_template("My name is {{ name }}", {"name": "James"})
@@ -7,11 +9,45 @@ def render_template(string, context):
     >>> render_template("User is {{ age }} years old", {"name": "James", "age" : 25})
     'User is 25 years old'
     """
-    for variable in context:
-        string = string.replace("{{ "+variable+" }}",str(context[variable]))
-    
+
+       
     return string
 
+
+class Parser():
+    def __init__(self, characters: str):
+        self._characters = characters
+        self._length = len(characters)
+        self._upto = 0
+
+    def end(self):
+        return self._upto == self._length
+
+    def peek(self):
+        return None if self.end() else self._characters[self._upto]
+
+    def next(self):
+        if not self.end():
+            self._upto += 1
+
+    def _parse_group(self):
+        nodes = []
+        if self.peek() != '{':
+            # we know this is a text node
+            nodes.append(self._parse_text())
+            print(nodes[0].text)
+        return GroupNode(nodes)
+
+    def _parse_text(self):
+        node = ''
+        while self.peek() != '{' and not self.end():
+            node += self._characters[self._upto]
+            self.next()
+        return TextNode(node)
+
 if __name__ == '__main__':
-    import doctest
-    doctest.testmod()
+    #import doctest
+    #doctest.testmod()
+    node = Parser("My name is {{ name }}")
+    print(node._characters)
+    print(node._parse_group().nodes)
