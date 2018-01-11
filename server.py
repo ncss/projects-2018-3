f2bcde615da1daa6c07fac4147b2df91a0ea7b30
dtuazon <35316008+dtuazon@users.noreply.github.com>
2018-01-11 16:25:51 +1100

    Adds template engine integration

--- server.py
+++ server.py
@@ -1,12 +1,20 @@
 from tornado.ncss import Server, ncssbook_log
 import tornadotesting
+from template import render_template
 
 def view_profile(request):
     """
     >>> tornadotesting.run(view_profile)
-    'Hi'
+    'My name is Isaac'
     """
-    request.write('Hi')
+
+    template = "My name is {{ name }}"
+    context = {'name':'Isaac'}
+
+    request.write(render_template(template, context))
+    
+
+    
 
 
 if __name__ == '__main__':
