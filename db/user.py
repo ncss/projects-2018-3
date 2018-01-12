b2b14a188c5f89883cc15d31b97d1d9d21d8bf20
snudmeh <snudmeh@hotmail.com>
2018-01-12 11:26:59 +1100

    modified user object. Added more attributes onto user. Added docstring explanations onto get_all and get_by_username

--- db/user.py
+++ db/user.py
@@ -1,13 +1,29 @@
 class User:
 	def __init__(self):
+		self.id = "0"
 		self.username = "sandy" 
+		self.password = "1234"
+		self.description = "sample text"
+		self.location = "Sydney"
+		self.birthdate = "DD/MM/YYYY"
+		self.image = "image.img"
 	
 	@staticmethod
 	def get_all():
+		''' This method gets a list of all users. 
+		returns	
+			list of user objects (list)
+		'''
 		return [User()]
 	
 	@staticmethod
 	def get_by_username(username : str):
+		''' This method gets a user by their username. 
+		arguments
+			- username(str)
+		returns
+			user object (User)
+		'''
 		return User()
 	
 	
