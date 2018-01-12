ebd4a7b41fb4714610201a8fbe73b2b651269315
Heartlessotfu <sliu3329@SHARED.SYDNEY.EDU.AU>
2018-01-12 12:41:30 +1100

    added test
    warning - launches infinite loop

--- template.py
+++ template.py
@@ -9,6 +9,7 @@ def render_template(string, context):
     'James is 25 years old'
     >>> render_template("User is {{ age }} years old", {"name": "James", "age" : 25})
     'User is 25 years old'
+    >>> render_template("{% if x %} x is True! {% end if %}", {'x': True})
     """
     node = Parser(string)._parse_group()
     return node.render(context)
